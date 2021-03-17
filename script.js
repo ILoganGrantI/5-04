12537330695203bf50f2bb5ad8a15430216095bf
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-17 15:11:59 +0000

    5-04

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // this changes the image
 document.getElementById('change-image').addEventListener('click', image)
-//this changes the text
+// this changes the text
 document.getElementById('change-text').addEventListener('click', text)
-//This hides the text
+// This hides the text
 document.getElementById('hide').addEventListener('click', hide)
-//this shows the text
+// this shows the text
 document.getElementById('show').addEventListener('click', show)
-//this makes the background black
+// this makes the background black
 document.getElementById('bg').addEventListener('click', background)
 
 function image () {
