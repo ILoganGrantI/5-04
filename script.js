499682c117eea84f587fc675594a84174f4a3660
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-17 14:48:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('change-image').addEventListener('click', image)
+document.getElementById('change-text').addEventListener('click', text)
+document.getElementById('hide').addEventListener('click', hide)
+document.getElementById('show').addEventListener('click', show)
+document.getElementById('bg').addEventListener('click', background)
+
+function image () {
+  document.getElementById('picture').src = './Images/4.gif'
+}
+
+function text () {
+  document.getElementById('text').innerHTML = 'Somthing'
+}
+
+function hide () {
+  document.getElementById('text').style.display = 'none'
+}
+
+function show () {
+  document.getElementById('text').style.display = 'block'
+}
+
+function background () {
+  document.body.style.backgroundColor = 'black'
+  document.getElementById('text').style.color = 'white'
+}
